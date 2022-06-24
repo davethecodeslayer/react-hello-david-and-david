a62e095081fdd3d09147832922ae28e0c389ae5f
edwindvd <edwindavidcr7@gmail.com>
2022-06-24 00:42:15 +0000

    ahi vamos

---
+++ src/js/component/cardlist.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Cardlist = () => {
+  return (
+    <div>cardlist</div>
+  )
+}
+export default Cardlist; 
---
+++ src/js/component/jumbotron.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Jumbotron = () => {
+  return (
+    <div>jumbotron</div>
+  )
+}
+export default Jumbotron;
